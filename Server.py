27b1118b4c6ac477bd5e4d4a9c9fe76f4d0a113a
liam <liamshani14@gmail.com>
2021-10-08 21:57:14 +0300

    Networking - only 1 client

--- Server.py
+++ Server.py
@@ -8,58 +8,54 @@ def main():
     md5_s = md5(start_string.encode())
     print("hash - " + md5_s.hexdigest())
     size = len(start_string)
+    print("Setting up server...")
     server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', 8820))
     server_socket.listen()
+    print("Listening...")
     messages_to_send = []
     client_sockets = []
-    data = ""
     while True:
         r_list, w_list, e_list = select.select([server_socket] + client_sockets, [], [], 0.01)
         for sock in r_list:
             if sock == server_socket:
                 connection, client_address = sock.accept()
                 client_sockets.append(connection)
-                s = f"Size of string is + {size}"
+                s = f"Size of string is - {size}"
                 length = str(len(s))
                 message = length.zfill(3) + s
-                print(message)
-                messages_to_send.append((sock, message))
-
+                messages_to_send.append((connection, message))
             else:
                 try:
-                    length = sock.recv(2).decode()
+                    length = sock.recv(3).decode()
                     try:
-                        data = sock.recv(int(length)).decode()
-                        
+                        data = sock.recv(int(length))
+                        if md5(data).hexdigest() == md5_s.hexdigest():
+                            s = "correct"
+                            length = str(len(s))
+                            message = length.zfill(3) + s
+                            messages_to_send.append((sock, message))
+                        else:
+                            s = "wrong"
+                            length = str(len(s))
+                            message = length.zfill(3) + s
+                            messages_to_send.append((sock, message))
+
                     except ValueError:
                         client_sockets.remove(sock)
                         sock.close()
-                    data = sock.recv(length).decode()
-                    if md5(data).hexdigest() == md5_s.hexdigest():
-                        s = "correct"
-                        length = str(len(s))
-                        message = length.zfill(3) + s
-                        messages_to_send.append((sock, message))
 
                 except ConnectionResetError:
                     client_sockets.remove(sock)
                     sock.close()
 
-            for message in messages_to_send:
-                current_socket, data = message
-                for sok in client_sockets:
-                    if sok == current_socket:
-                        sok.send(data.encode())
-            messages_to_send = []
-
-        solve = "0" * size
-        while True:
-            if md5_s.hexdigest() == md5(solve.encode()).hexdigest():
-                print(solve + " is the string!")
-                break
-            solve = str(int(solve) + 1)
-            solve = solve.zfill(size)
+        for message in messages_to_send:
+            current_socket, data = message
+            for sok in client_sockets:
+                if sok == current_socket:
+                    print(data[3:])
+                    sok.send(data.encode())
+        messages_to_send = []
 
 
 if __name__ == "__main__":
--- client.py
+++ client.py
@@ -1,14 +1,30 @@
-import threading
-import multiprocessing
 import socket
 
-def connect_to_server():
-    sock = socket.socket()
-    sock.connect(('127.0.0.1', 8820))
-def create_message():
+
+def create_message(size, s):
+    s = str(int(s) + 1)
+    s = s.zfill(size)
+    return s
+
 
 def main():
-
+    print("Connecting...")
+    sock = socket.socket()
+    sock.connect(('127.0.0.1', 8820))
+    print("Connected!")
+    length = sock.recv(3).decode()
+    data = sock.recv(int(length)).decode()
+    size = int(data.split("Size of string is - ")[1])
+    s = "0" * size
+    while data == "wrong" or " - " in data:
+        print(data)
+        s = create_message(size, s)
+        length = str(len(s))
+        message = length.zfill(3) + s
+        sock.send(message.encode())
+        length = sock.recv(3).decode()
+        data = sock.recv(int(length)).decode()
+    print(data)
 
 
 if __name__ == "__main__":
