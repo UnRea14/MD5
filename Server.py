a4c1982f3c77271dd81b642942e04164e23ded87
liam <liamshani14@gmail.com>
2021-10-08 11:15:50 +0300

    include main

--- Server.py
+++ Server.py
@@ -1,13 +1,19 @@
 from hashlib import md5
 
-s = input("Enter a string: ")
-md5_s = md5(s.encode())
-print("hash - " + md5_s.hexdigest())
-size = len(s)
-solve = "0" * size
-while True:
-    if md5_s.hexdigest() == md5(solve.encode()).hexdigest():
-        print(solve + " is the string!")
-        break
-    solve = str(int(solve) + 1)
-    solve = solve.zfill(size)
+
+def main():
+    s = input("Enter a string: ")
+    md5_s = md5(s.encode())
+    print("hash - " + md5_s.hexdigest())
+    size = len(s)
+    solve = "0" * size
+    while True:
+        if md5_s.hexdigest() == md5(solve.encode()).hexdigest():
+            print(solve + " is the string!")
+            break
+        solve = str(int(solve) + 1)
+        solve = solve.zfill(size)
+
+
+if __name__ == "__main__":
+    main()
